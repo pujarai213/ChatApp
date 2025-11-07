9cd1a86deda5866086564d8be5b7bf554c734ffa
Puja Rai <rpuja7700@gmail.com>
2025-11-07 23:26:53 +0545

    login and logout functionality added

---
+++ backend/src/lib/env.js
@@ -0,0 +1,8 @@
+import "dotenv/config";
+
+export const ENV = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+};
